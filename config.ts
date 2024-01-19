dde5dc355558b8c0cde7619c3d1a837211a878cc
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2024-01-19 17:32:25 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: process.env.API_ENDPOINT || 'https://api.gabodev.com',
+    API_ENDPOINT: process.env.API_ENDPOINT || 'https://api.gabodev.com/goodgifts',
   };
   
   export default config;
